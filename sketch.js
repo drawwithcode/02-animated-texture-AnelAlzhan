836ee8c3bccaad9cb5623adb34f4101499d59958
AnelAlzhan <anelya99@gmail.com>
2021-10-13 00:42:16 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,27 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  translate(windowWidth / 2, windowHeight / 2);
+  frameRate(10);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  circles();
+}
+
+function circles() {
+  for (let i = 0; i < 464; i++) {
+    if (mouseX < 300 && mouseX > 300) {
+      fill(255, 255, 255);
+    }
+
+    if (mouseX < random() * 1000) {
+      fill(0, 0, 0);
+    }
+
+    if (mouseX < random() * 700) {
+      fill(249, 178, 72);
+    }
+
+    ellipse(random(windowWidth), random(windowHeight), random(50));
+  }
 }
